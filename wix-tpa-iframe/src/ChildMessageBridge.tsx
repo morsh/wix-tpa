1cec98e6b958bca5e88326cb672610d9c17c7260
Mor Shemesh <morshe@wix.com>
2022-03-22 11:08:39 +0200

    working sample

--- wix-tpa-iframe/src/ChildMessageBridge.tsx
+++ wix-tpa-iframe/src/ChildMessageBridge.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { proxy, windowEndpoint } from 'comlink';
+import { proxy, windowEndpoint, wrap,  } from 'comlink';
 
 export const ChildMessageBridge = () => {
   const [state, setState] = useState('');
   useEffect(() => {
     console.log('...2');
-    const api = proxy(windowEndpoint(window.parent)) as any;
+    const api = wrap(windowEndpoint(window.parent)) as any;
     console.log('...3', api);
     api.sayHello().then((msg: string) => setState(msg));
   }, []);
